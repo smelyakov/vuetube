f370d8f3e9d4e71a30a76ad0f2070cb39df891f1
Alexander Smelyakov <asmelyakov@lineate.com>
2018-06-26 10:14:35 +0600

    add watch view

---
+++ src/views/watch/Watch.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="watch">
+    Watch Page
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Watch',
+}
+</script>
+
+<style lang="scss">
+.watch {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
+</style>
+
