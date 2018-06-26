7c4ac71c00865cf97b97be17be8d9e5bd777cabf
Alexander Smelyakov <asmelyakov@lineate.com>
2018-06-26 10:59:27 +0600

    setup watch layout

--- src/views/watch/Watch.vue
+++ src/views/watch/Watch.vue
@@ -1,21 +1,49 @@
 <template>
   <div class="watch">
-    Watch Page
+    <header class="watch__header">
+      <SearchBox />
+    </header>
+    <div class="watch__player">
+      <Player />
+    </div>
+    <div class="watch__content">
+      <section class="watch__info">
+        <h1 class="watch__title">Video Title</h1>
+        <h1 class="watch__views">100 просмотров</h1>
+      </section>
+      <section class="watch__comments"></section>
+      <aside class="watch__related"></aside>
+    </div>
   </div>
 </template>
 
 <script>
+import SearchBox from '@/components/SearchBox'
+import Player from './components/Player'
+
 export default {
   name: 'Watch',
+
+  components: {
+    Player,
+    SearchBox
+  }
 }
 </script>
 
 <style lang="scss">
 .watch {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
+  &__header {
+    padding: .5rem 4%;
+  }
+
+  &__content {
+    padding: 1.5rem 4%;
+  }
+
+  &__title {
+    font-size: 18px;
+    font-weight: 600;
+  }
 }
 </style>
-
---
+++ src/views/watch/components/Player.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="player">
+    <span class="player__play-button"></span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Player'
+}
+</script>
+
+<style lang="scss">
+.player {
+  position: relative;
+  width: 100%;
+  height: calc(0.5625 * 100vw);
+  max-height: calc(100vh - 185px);
+  background: #eaeaea;
+
+  &__play-button {
+    display: flex;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: 96px;
+    height: 96px;
+    align-items: center;
+    justify-content: center;
+    transform: translate3d(-50%, -50%, 0);
+    border-radius: 50%;
+    background: white;
+
+    &:before {
+      content: '';
+      display: block;
+      margin-left: 10px;
+      border-color: transparent transparent transparent #ccc;
+      border-width: 20px 0 20px 32px;
+      border-style: solid;
+    }
+  }
+
+}
+</style>
