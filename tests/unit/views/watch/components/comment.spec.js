16c5e1f7e674a34c74fed1b76c146b07ef27de6b
Alexander Smelyakov <asmelyakov@lineate.com>
2018-07-24 11:02:59 +0600

    more tests on comment

--- tests/unit/views/watch/components/comment.spec.js
+++ tests/unit/views/watch/components/comment.spec.js
@@ -14,17 +14,14 @@ describe('Comment.vue', () => {
         authorChannelId: { value: 'UCd_mRRYGpg_R5d1kZ8hL7-Q' },
         authorChannelUrl: 'http://www.youtube.com/channel/UCd_mRRYGpg_R5d1kZ8hL7-Q',
         authorDisplayName: 'Google User',
-        authorProfileImageUrl: 'https://yt3.ggpht.com/-PAWaHDnWQns/AAAAAAAAAAI/AAAAAAAAAAA/dW1e4b-fKMM/s28-c-k-no-mo-rj-c0xffffff/photo.jpg',
-        canRate: true,
-        likeCount: 0,
-        publishedAt: '2016-07-22T12:26:45.000Z',
+        authorProfileImageUrl: '//yt3.ggpht.com/s28-c-k-no-mo-rj-c0xffffff/photo.jpg',
+        publishedAt: '2016-07-22T12:26:45',
         textDisplay: 'Hell yeah! The Best. Remember.',
         textOriginal: 'Hell yeah! The Best. Remember.',
         updatedAt: '2016-07-22T12:26:45.000Z',
-        videoId: 'WensdPz9cqM',
-        viewerRating: 'none'
       }
     }
+
     wrapper = shallowMount(Comment, {
       propsData: { comment }
     })
@@ -32,5 +29,37 @@ describe('Comment.vue', () => {
 
   it('renders a comment', () => {
     expect(wrapper.contains('.comment')).toBe(true)
+  })
+
+  describe('author', () => {
+    it('returns comment author', () => {
+      expect(wrapper.vm.author).toMatch('Google User')
+    })
+  })
+
+  describe('avatar', () => {
+    it('returns larger author avatar', () => {
+      expect(wrapper.vm.avatar).toMatch('//yt3.ggpht.com/s48-c-k-no-mo-rj-c0xffffff/photo.jpg')
+    })
+  })
+
+  describe('publishedAt', () => {
+    it('returns formatted date', () => {
+      expect(wrapper.vm.publishedAt).toMatch('22 July 2016, 12:26')
+    })
+  })
+
+  describe('textDisplay', () => {
+    it('returns comment text', () => {
+      expect(wrapper.vm.textDisplay).toMatch('Hell yeah! The Best. Remember.')
+    })
+  })
+
+  describe('bigAvatar', () => {
+    it('makes 28px avatar to be 48px', () => {
+      const avatar = '//host/s28-c-k-no-mo-rj-c0xffffff/photo.jpg'
+
+      expect(wrapper.vm.bigAvatar(avatar)).toMatch('//host/s48-c-k-no-mo-rj-c0xffffff/photo.jpg')
+    })
   })
 })
