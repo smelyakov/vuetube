de851250142fbb2a450ecc5384aac4713537db79
Alexander Smelyakov <asmelyakov@lineate.com>
2021-10-16 21:04:51 +0600

    update deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,9 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    browser: true
   },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
+  extends: ['plugin:vue/essential', '@vue/standard'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
