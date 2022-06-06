e11d4fc981f903329775919afbc69aaf5e94a3e8
Alexander Smelyakov <asmelyakov@lineate.com>
2022-06-06 22:58:25 +0600

    ts utils

--- src/utils/fetch-utils.ts
+++ src/utils/fetch-utils.ts
@@ -1,10 +1,9 @@
-export const handleJSON = (response) => {
+export const handleJSON = async (response: Response) => {
   const contentType = response.headers.get('content-type')
 
   if (contentType && contentType.includes('application/json')) {
-    return response.json().then((json) => {
-      return response.ok ? json : Promise.reject(json)
-    })
+    const json = await response.json()
+    return response.ok ? json : Promise.reject(json)
   }
 
   return Promise.reject(new Error('Oops, we haven`t got JSON!'))
